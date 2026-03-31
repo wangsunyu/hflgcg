8cc205f8e4b6fa4622693b87ab8d3f38bd78b966
sywang19 <sywang19@iflytek.com>
2026-03-31 19:57:08 +0800

    Initial Commit

---
+++ HFITCampus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HFITCampus
+//
+//  Created by wsy on 2026/3/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HFITCampus/HFITCampusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HFITCampusApp.swift
+//  HFITCampus
+//
+//  Created by wsy on 2026/3/31.
+//
+
+import SwiftUI
+
+@main
+struct HFITCampusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
